f6f669edde11345e87276bfc33e918cf83779f46
Vitalii <krava18@ukr.net>
2016-04-03 21:30:17 +0300

    FInish order

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items do
-      put 'decrement', on: :member
-    end
+    put 'decrement', on: :member
+  end
   resources :carts
   get 'store/index'
 
   root to: 'store#index', as: 'store'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
