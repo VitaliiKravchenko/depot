41b055fe2c1a4e157d8e671bf5ebb61d668ef0b2
VitaliiKravchenko <krava18@ukr.net>
2016-05-06 15:39:33 +0300

    Link to image and scss customization

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,14 +3,18 @@ class Product < ActiveRecord::Base
 	has_many :line_items
 	has_many :orders, through: :line_items
 	before_destroy :ensure_not_referenced_by_any_line_item
-	#validates :title, :description, :image_url, presence: true
-	#validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	#validates :title, uniqueness: true
-	#validates :image_url, uniqueness: true
-	#validates :image_url, allow_blank: true, format: {
-	#	with: %r{\.(gif|jpg|png)\Z}i,
-	#	message: 'URL должен указывать на изображение формата GIF, JPG или PNG'
-	#}
+	validates :title, :description, :image, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_uid, uniqueness: true
+  #validates :image_uid, allow_blank: true, format: {
+  #                             		with: %r{\.(gif|jpg|png)\Z}i,	
+  #                             		message: 'Зображення має бути формату GIF, JPG або PNG'
+  #                             			}
+  validates_size_of :image, maximum: 2000.kilobytes,
+                  message: "should be no more than 2000 KB", if: :image_changed?
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false,
+                   message: "має бути формату .jpeg, .jpg, .png, .bmp", if: :image_changed?
 
 	def self.latest
 		Product.order(:updated_at).last
